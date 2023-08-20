757cf73d17a77aae721716b712cf9fa6969eb37a
ropaolle1~ <ropaolle@gmail.com>
2023-08-20 13:06:32 +0200

    confirm dialog

--- src/app/(protected)/species/page.tsx
+++ src/app/(protected)/species/page.tsx
@@ -1,15 +1,12 @@
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { type Database } from "@/lib/database.types";
+import createServerComponentClientWithCookies from "@/lib/createServerComponentClientWithCookies";
 import SpeciesTable from "./SpeciesTable";
 
 export default async function Images() {
-  const supabase = createClientComponentClient<Database>();
+  const supabase = await createServerComponentClientWithCookies();
   const { data } = await supabase.from("species").select(`
     *,
     images (thumbnail_url)
   `);
-
-  // console.log("rows", data);
 
   return <>{data && <SpeciesTable rows={data} />}</>;
 }
--- src/components/Table/setupAction.ts
+++ src/components/Table/setupAction.ts
@@ -1,9 +1,8 @@
-export default function setupAction(
-  action: string,
-  func: (id: string) => void,
-) {
+export default function setupAction(action: string, func: (id: string) => void, confirm = false) {
+  if (typeof func !== "function") return;
+
   document.querySelectorAll(`.${action}-button`).forEach((button) => {
-    button.addEventListener<any>("click", (e) => {
+    button.addEventListener<any>(confirm ? "confirm.te.popconfirm" : "click", (e) => {
       e.stopPropagation();
       const index = button.getAttribute("data-te-index");
       if (index && index.length) {
