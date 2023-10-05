16857b78d8d099700db1743e028533b666c0962a
ropaolle <ropaolle@gmail.com>
2023-10-05 16:12:48 +0200

    remove broadcast, for now

--- src/supabase/database/use-delete-species-mutation.ts
+++ src/supabase/database/use-delete-species-mutation.ts
@@ -1,16 +1,13 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteSpeciesById } from "./queries/delete-species-by-id";
 import useSupabase from "./use-supabase";
-import { useBroadcast } from "@/hooks/useBroadcast";
 
 export default function useDeleteSpeciesMutation() {
   const client = useSupabase();
   const queryClient = useQueryClient();
-  const broadcast = useBroadcast("app", "updates");
 
   return useMutation(
-    async ({ id, brodcastMessage }: { id: string; brodcastMessage?: unknown }) => {
-      broadcast(brodcastMessage || id, "species-deleted");
+    async ({ id }: { id: string }) => {
       return deleteSpeciesById(client, id).then((result) => result.data);
     },
     {
--- src/supabase/database/use-upsert-species-mutation.ts
+++ src/supabase/database/use-upsert-species-mutation.ts
@@ -1,4 +1,3 @@
-import { useBroadcast } from "@/hooks/useBroadcast";
 import type { Species } from "@/types/app.types";
 import { useMutation } from "@tanstack/react-query";
 import { upsertSpeciesById } from "./queries/upsert-species-by-id";
@@ -6,10 +5,8 @@ import useSupabase from "./use-supabase";
 
 export default function useUpsertSpeciesMutation() {
   const client = useSupabase();
-  const broadcast = useBroadcast("app", "updates");
 
   return useMutation(async (data: Partial<Species>) => {
-    broadcast(data, data?.id ? "species-updated" : "species-created");
     return upsertSpeciesById(client, data).then((result) => result.data);
   });
 }
